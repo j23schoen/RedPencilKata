9745ee0940842ca7764458158a36137a7c9e5cd1
j23schoen <j23schoen@gmail.com>
2016-04-06 21:20:34 -0400

    duration variable

--- src/RedPencil.java
+++ src/RedPencil.java
@@ -4,12 +4,20 @@ import java.math.RoundingMode;
 public class RedPencil {
 
     private BigDecimal price;
+    private int durationOfPromotion;
 
     public RedPencil(String productPrice) {
         this.price = new BigDecimal(productPrice);
+        this.durationOfPromotion = 0;
     }
 
     public double getPrice(){
         return price.setScale(2, RoundingMode.HALF_EVEN).doubleValue();
     }
+
+    public int getDuration(){
+        return durationOfPromotion;
+    }
+
+
 }
--- test/RedPencilTests.java
+++ test/RedPencilTests.java
@@ -6,7 +6,12 @@ public class RedPencilTests {
     @Test
     public void priceWillReturnTheRightPriceInput(){
         RedPencil test = new RedPencil("10.50");
-        Assert.assertEquals(10.50, test.getPrice(), 0.01);
+        Assert.assertEquals(10.50, test.getPrice(), 0);
     }
 
+    @Test
+    public void returnDuration(){
+        RedPencil test = new RedPencil("3");
+        Assert.assertEquals(0, test.getDuration());
+    }
 }
