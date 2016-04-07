dc48880eafdcac6182fa2d76e0df1068b60fdb37
j23schoen <j23schoen@gmail.com>
2016-04-07 12:04:08 -0400

    refactored names of prices

--- src/RedPencil.java
+++ src/RedPencil.java
@@ -3,17 +3,19 @@ import java.math.RoundingMode;
 
 public class RedPencil {
 
-    private BigDecimal price;
+    private BigDecimal originalPrice;
+    private BigDecimal redPencilPrice;
     private int durationOfPromotion;
+    private int durationOfRedPencil;
 
-    public RedPencil(String originalPrice) {
-        this.price = new BigDecimal(originalPrice);
+    public RedPencil(String originaloriginalPrice) {
+        this.originalPrice = new BigDecimal(originaloriginalPrice);
         this.durationOfPromotion = 0;
     }
 
-    public double getPrice(){
-        System.out.println("price");
-        return price.setScale(2, RoundingMode.HALF_EVEN).doubleValue();
+    public double getoriginalPrice(){
+        System.out.println("originalPrice");
+        return originalPrice.setScale(2, RoundingMode.HALF_EVEN).doubleValue();
     }
 
     public int getDuration(){
@@ -28,35 +30,39 @@ public class RedPencil {
         durationOfPromotion = 0;
     }
 
-    public boolean checkFor30StablePrice(){
+    public boolean checkFor30StableoriginalPrice(){
         if(durationOfPromotion >= 30){
-            System.out.println("price stable");
+            System.out.println("originalPrice stable");
             return true;
         }
         else{
-            System.out.println("price not stable");
+            System.out.println("originalPrice not stable");
             return false;
         }
     }
 
-    public double reducePrice(String reductionPercentage){
+    public double reduceoriginalPrice(String reductionPercentage){
         BigDecimal percentToReduceBy = new BigDecimal(reductionPercentage);
 
-        if(checkFor30StablePrice()){
+        if(checkFor30StableoriginalPrice()){
             if(percentToReduceBy.doubleValue() >= .05 && percentToReduceBy.doubleValue() <= .30){
-                BigDecimal percentageOfPrice = price.multiply(percentToReduceBy);
-                price = price.subtract(percentageOfPrice);
+                BigDecimal percentageOfOriginalPrice = originalPrice.multiply(percentToReduceBy);
+                redPencilPrice = originalPrice.subtract(percentageOfOriginalPrice);
             }
-            System.out.println("price: $" + price);
-            return price.setScale(2, RoundingMode.HALF_EVEN).doubleValue();
+            System.out.println("originalPrice: $" + originalPrice);
+            return redPencilPrice.setScale(2, RoundingMode.HALF_EVEN).doubleValue();
         }
         else{
-            System.out.println("price cannot be reduced because the previous price hasn't been stable for 30 days");
-            System.out.println("price: $" + price);
-            return price.setScale(2, RoundingMode.HALF_EVEN).doubleValue();
+            System.out.println("originalPrice cannot be reduced because the previous originalPrice hasn't been stable for 30 days");
+            System.out.println("originalPrice: $" + originalPrice);
+            return originalPrice.setScale(2, RoundingMode.HALF_EVEN).doubleValue();
         }
     }
 
+    public void addDaysToRedPencilDuration(int numberOfDays){
+        durationOfRedPencil += numberOfDays;
+    }
 
+    
 
 }
